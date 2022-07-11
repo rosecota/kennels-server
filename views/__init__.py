b73c1fed546285a46921485a73917398527d42b1
Rose Cota <rosejcota@gmail.com>
2022-07-11 14:53:33 -0500

    setup and initial animals locations and employees get requests

---
+++ views/__init__.py
@@ -0,0 +1,3 @@
+from .animal_requests import get_all_animals
+from .employee_requests import get_all_employees
+from .location_requests import get_all_locations
---
+++ views/location_requests.py
@@ -0,0 +1,16 @@
+LOCATIONS = [
+    {
+        "id": 1,
+        "name": "Nashville North",
+        "address": "8422 Johnson Pike"
+    },
+    {
+        "id": 2,
+        "name": "Nashville South",
+        "address": "209 Emory Drive"
+    }
+]
+
+
+def get_all_locations():
+    return LOCATIONS
